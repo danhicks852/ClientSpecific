88412374f50130974162bdd928155a946a5c097c
danhicks852 <96134901+danhicks852@users.noreply.github.com>
2022-03-07 08:27:38 -0500

    Co-authored-by: Stephen Nix <stephen.nix@provaltech.com>

--- ClientRequests/In Progress/Deploy-AnyConnectFull.ps1
+++
@@ -1,9 +0,0 @@
-#urls
-#installer files:
-##https:\\file.provaltech.com/repo/kaseya/clients/altourage/cisco/installername.msi
-#json
-##https://file.provaltech.com/repo/kaseya/clients/altourage/cisco/json/<client>/OrgInfo.json
-
-<#
-https://www.cisco.com/c/en/us/td/docs/security/vpn_client/anyconnect/anyconnect49/administration/guide/b_AnyConnect_Administrator_Guide_4-9/deploy-anyconnect.html#ID-1425-0000015f
-#>
